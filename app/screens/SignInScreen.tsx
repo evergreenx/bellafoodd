7feb20733fb7b65baef46770f7b23bb14f6b3381
ido evergeen <dicksonevegreenido@gmail.com>
2023-03-23 22:16:27 +0100

    add auth page tab

--- app/screens/SignInScreen.tsx
+++ app/screens/SignInScreen.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, View, Image } from "react-native"
+import { ViewStyle, View, Image, TextStyle, Button, TouchableOpacity } from "react-native"
 import { AppStackScreenProps } from "../navigators"
 import { Screen, Text } from "../components"
 import { colors, spacing } from "../theme"
@@ -24,48 +24,100 @@ export const SignInScreen: FC<SignInScreenProps> = observer(function SignInScree
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const [tab, setTab] = React.useState(1)
 
   const logo = require("../../assets/images/logoxx.png")
+
+  const setActiveTab = (tab: number) => {
+    setTab(tab)
+  }
+
   return (
     <Screen
       style={$root}
-      preset="scroll"
       contentContainerStyle={$screenContentContainer}
-      safeAreaEdges={["top", "left"]}
+      safeAreaEdges={["right", "left"]}
     >
       <View style={$topContainer}>
-        <Image source={logo} />
+        <View style={$logoContainer}>
+          <Image source={logo} />
+        </View>
 
         <View style={$tabContainer}>
-          <View>
-            <Text>Login</Text>
-          </View>
+          <TouchableOpacity onPress={() => setActiveTab(1)}>
+            <Text style={$tabText}>Login</Text>
+          </TouchableOpacity>
 
-          <View>
-            <Text>signup</Text>
-          </View>
+          <TouchableOpacity onPress={() => setActiveTab(2)}>
+            <Text style={$tabText}>signup</Text>
+          </TouchableOpacity>
         </View>
+      </View>
+
+      <View style={$formContainer}>
+        {tab === 1 ? <Text>Tab 1</Text> : <Text>Tab 2</Text>}
+  
       </View>
     </Screen>
   )
 })
 
 const $root: ViewStyle = {
-  flex: 1,
-  backgroundColor: colors.palette.brandColorBg,
+  // backgroundColor: colors.palette.brandColorBg,
+  backgroundColor: colors.palette.neutral100,
+  height: "100%",
+}
+
+const $screenContentContainer: ViewStyle = {
+  backgroundColor: colors.palette.neutral100,
+}
+
+const $topContainer: ViewStyle = {
+  padding: spacing.large,
+  justifyContent: "space-around",
+  // alignItems: "center",
+  // flexDirection: "column",
+  height: 382,
+  width: "100%",
+  borderRadius: spacing.huge,
+  zIndex: 2,
+  backgroundColor: colors.palette.neutral100,
+  position: "absolute",
+}
+
+const $tabText: TextStyle = {
+  color: colors.palette.neutral900,
+  fontSize: 18,
+  fontWeight: "600",
+  textTransform: "capitalize",
+}
+
+const $logoContainer: ViewStyle = {
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: 90,
 }
 
 const $tabContainer: ViewStyle = {
   flex: 1,
-
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "flex-end",
 }
-const $screenContentContainer: ViewStyle = {}
 
-const $topContainer: ViewStyle = {
-  flex: 1,
-
-  // backgroundColor: colors.palette.neutral100,
-  backgroundColor: colors.palette.brandColor,
-  height: 382,
+const $formContainer: ViewStyle = {
+  backgroundColor: colors.palette.brandColorBg,
+  height: 900,
+  width: "100%",
   borderRadius: spacing.huge,
+  zIndex: 1,
+  // flex:  1,
+  justifyContent: "center",
+  alignItems: "center",
 }
+
+// const $tt: TextStyle = {
+//   color: "red",
+//   fontSize: 18,
+//   fontWeight: "600",
+// }
